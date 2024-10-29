6641f958f0861d6738f217e7a98e60528ee9d650
TimothyMuigai <timothy.muigai@strathmore.edu>
2024-10-29 09:37:12 +0300

    made changes to index.html, app.js and style.css

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,6 @@
+
+
+
 function fetchApi(){
     const url = 'http://localhost:3000/customers';
 
@@ -32,23 +35,28 @@ const noItemsContainer = document.querySelector('.no-items');
 function displayCartItems(details){
     tableContent.style.display = "block";
     noItemsContainer.style.display = "none";
+
     details.forEach(customer => {        
         const itemCard = document.createElement('tr');
         itemCard.classList.add('pro-card');
         itemCard.innerHTML=`
             <td data-cell="Product" class="images">
-                <img src="${customer.product_img}" alt="product image">
+                <img src="../${customer.product_img}" alt="product image">
             </td>
             <td class="details" data-cell="Details">
                 <p id="item-detail">${customer.details}</p>
-                <p id="item-price">$${customer.price}</p>
+                <p id="item-price">Price: $${customer.price}</p>
                 <p><button class="Del">Remove product</button></p>
             </td>
             <td data-cell="No. of items" class="input">
                 <input type="number" value="${customer.quantity}" min="0" id="quantity">
             </td>
-            <td data-cell="sub-total" class="price item">$0.00</td>
+            <td data-cell="sub-total" class="price item">$${customer.quantity*customer.price}</td>
         `;
         document.querySelector('tbody').appendChild(itemCard);
     })
+}
+
+function calTotal(){
+    
 }
